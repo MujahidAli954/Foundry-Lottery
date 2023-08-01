2c7034933fb363d7ad26e89767eaa5061d78b3f1
mujahidali <mujahidtech985@gmail.com>
2023-08-02 01:44:48 +0500

    create randomness function

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,19 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+
 contract Lottery {
+    /** error */
     error Lottery__NotEnoughEthSent();
+
+
+    /**State variables */
+    uint256 private constant REQUEST_CONFIRMATIONS = 3;
+    uint256 private constant NUM_WORDS=1;
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
+    address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64  private immutable i_subscriptionId;
+    uint256 private immutable i_callbackGasLimit;
     address payable[]  private s_players;
 
 
     /** Events */
     event EnteredLottery(address indexed player);
 
-    constructor(uint256 entranceFee ,uint256 interval){
+    constructor(uint256 entranceFee ,uint256 interval,address vrfCoordinator,bytes32 gasLane,uint64 subscriptionId,uint256 callbackGasLimit){
         i_entranceFee = entranceFee;
         i_interval = interval;
+        i_vrfCoordinator = vrfCoordinator;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterlottery() public payable{
@@ -26,7 +45,17 @@ contract Lottery {
     }
 
     
-    function pickWinner() public {
+    function pickWinner() external {
+        if((block.timestamp - s_lastTimeStamp) < i_interval){
+            revert();
+        }
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
     }
 
     /** Geters */
